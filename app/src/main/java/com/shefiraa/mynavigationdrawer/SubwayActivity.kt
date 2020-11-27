9fefb2d699c86daa4363e3a60b26a8c98a292bd0
shefiraa <shefiraharani4@gmail.com>
2020-11-27 17:53:34 +0700

    Navigation Drawer

---
+++ app/src/main/java/com/shefiraa/mynavigationdrawer/SubwayActivity.kt
@@ -0,0 +1,12 @@
+package com.shefiraa.mynavigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shefiraa.mynavigationdrawer.R
+
+class SubwayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
---
+++ app/src/main/java/com/shefiraa/mynavigationdrawer/ui/tool/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package com.shefiraa.mynavigationdrawer.ui.tool
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
